cd3d95f65042228e53e2de1f577013668a387ac9
Ricardo Marques <dataricardom@gmail.com>
2024-11-25 21:21:57 -0300

    Subindo exemplos 1 e 2

---
+++ exemplo_1.py
@@ -0,0 +1,5 @@
+print("Vasco da Gama")
+
+pontos = input("Quantos pontos o vasco tem? ")
+
+print("O vasco possui: " + pontos + " PTS")
---
+++ exemplo_2.py
@@ -0,0 +1,6 @@
+#Exemplo 2
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+print("A soma dos valores é: " + str(valor1 + valor2))
